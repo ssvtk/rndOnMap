32fb7d6cf6539917db483776cafd0654a0bc0e05
Slava <svtkjs@gmail.com>
2022-11-22 19:22:07 +0300

    user class added

---
+++ src/User.ts
@@ -0,0 +1,15 @@
+import { faker } from "@faker-js/faker"
+export class User {
+  name: string
+  location: {
+    lat: number
+    lng: number
+  }
+  constructor() {
+    this.name = faker.name.fullName()
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    }
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { User } from "./User"
+const newUser = new User()
+console.log(newUser)
